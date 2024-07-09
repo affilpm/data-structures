c4bb9196fbe2e67215a5651a24f33b0a1b944e0c
affilpm <affilpm2004@gmail.com>
2024-07-09 17:55:32 +0530

    updated singly ll

--- Data-Structure-1/Linked-Lists/singly-linked-list.py
+++ Data-Structure-1/Linked-Lists/singly-linked-list.py
@@ -2,12 +2,13 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
-        # self.pnext = None
         
+      
         
 class Linkedlist:
     def __init__(self):
         self.head = None
+        
         
         
 # print linked list        
@@ -31,7 +32,7 @@ class Linkedlist:
         
         
 
-#adding new node atend     
+#adding new node at end     
     def add_end(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -42,6 +43,7 @@ class Linkedlist:
                 n = n.next
             n.next = new_node  
             
+          
               
 # adding new after a node                                 
     def add_after(self,data,x):
@@ -57,6 +59,7 @@ class Linkedlist:
             new_node.next = n.next
             n.next = new_node      
             
+
 
 # adding new node before a node
     def add_before(self,data,x):
@@ -81,6 +84,35 @@ class Linkedlist:
             n.next = new_node
             
             
+            
+
+# adding new node in the middle  
+    def add_mid(self,data):
+        new_node = Node(data)
+        if self.head is None :
+            self.head = new_node
+            return
+        fast = self.head
+        slow = self.head
+        prev = self.head
+        while fast and fast.next:
+            fast = fast.next.next
+            prev = slow
+            slow = slow.next
+            
+        if prev is not None:
+            new_node.next = prev.next
+            prev.next = new_node   
+        else:
+            new_node.next = self.head
+            self.head = new_node                       
+
+
+
+
+
+
+
 # delete node at the beginning            
     def del_begin(self):
         if self.head is not None:
@@ -95,7 +127,41 @@ class Linkedlist:
         n.next = None
         
      
-# delete note by searching value        
+# del after a node
+    def del_after(self,x):
+        n = self.head
+        while n is not None:
+            if x == n.data:
+                break
+            n = n.next
+        if n is None:
+            print('no node you typed exist')
+        elif n.next is None:
+            print('no node', x)    
+        else:
+            n.next = n.next.next            
+
+
+# delete node in the middle
+    def del_mid(self):
+        if self.head is None or self.head.next is None:
+            print('not have enough node for deleting from middle')
+            return
+        fast = self.head
+        slow = self.head
+        prev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            prev = slow
+            slow = slow.next
+            
+        if prev:
+            prev.next = slow.next
+            
+            
+            
+                
+# delete node by searching value        
     def del_by_val(self,x):
         if self.head is None:
             print("Ll is empty")
@@ -163,25 +229,31 @@ def array_to_linked_list(arr):
 
 
 
-arr = [1, 3, 4, 5]
-linked_list = array_to_linked_list(arr)
-linked_list.print_LL()
+# arr = [1, 3, 4, 5]
+# linked_list = array_to_linked_list(arr)
+# linked_list.print_LL()
 
 LL1 = Linkedlist()
 
 LL1.add_begin(1)
 LL1.add_begin(10)
-LL1.add_begin(10)
-LL1.add_end(6)     
-LL1.add_end(1)
-LL1.add_after(9,1)
-LL1.add_begin(10)
-LL1.add_begin(10)
-LL1.del_end()
-LL1.del_begin()
-LL1.add_before(3,1)
-LL1.del_by_val(1)
+LL1.add_begin(6)
+LL1.del_after(6)
+
+# LL1.add_begin(7)
+# LL1.add_end(6)     
+# LL1.add_after(9,1)
+# LL1.add_mid(4)
+# LL1.add_begin(10)
+# LL1.add_begin(10)
+# LL1.del_end()
+# LL1.del_begin()
+# LL1.add_before(3,1)
+# LL1.del_by_val(1)
 # LL1.remove_duplicates()
+# LL1.del_after()
+
+
 # LL1.reverse()
 
 
