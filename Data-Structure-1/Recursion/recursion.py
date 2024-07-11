bd46c54f2fec8408c8ba1b3e7e286e93feb6001c
affilpm <affilpm2004@gmail.com>
2024-07-11 16:18:33 +0530

    updated

--- Data-Structure-1/Recursion/recursion.py
+++ Data-Structure-1/Recursion/recursion.py
@@ -36,6 +36,18 @@ print(sum_of_digits(1234))
 
 
 
+def sum_of_array(n):
+    if len(n) == 0:
+        return 0
+    else:
+        return n[0] + sum_of_array(n[1:])
+    
+    
+print(sum_of_array([1,2,3,4,5]))     
+
+
+
+
 
 def power(x, n):
     if n == 0:
@@ -50,15 +62,24 @@ print(power(2, 3))
                   
                   
                   
-                  
-                  
-def fact(n):
-    if n == 0:
-        return 1
+def reverse(n):
+    if len(n) == 0:
+        return " "
     else:
-        return n * fact(n-1)
-print(fact(5))
+        return n[-1] + reverse(n[:-1])
+print(reverse("hello"))  
 
+
+
+
+def palindrome(n):
+    if len(n) <= 1:
+        return True
+    elif n[0] != n[-1]:
+        return False
+    else:
+        return palindrome(n[1:-1])  
+print(palindrome("racecar"))    
 
 
 
