10b601b7d719d08e6c5665ad55e05ae4b9d83c60
affilpm <affilpm2004@gmail.com>
2024-07-11 16:58:33 +0530

    updated

--- Data-Structure-1/Search-Algorithms.py/binary-search.py
+++ Data-Structure-1/Search-Algorithms.py/binary-search.py
@@ -12,3 +12,43 @@ def binary_search(arr, target):
 
 h = [1,2,3,4,5,6]
 print(binary_search(h,5))            
+
+
+
+
+# find first occurence of target
+def binary_search1(arr,target):
+    left,right = 0, len(arr)-1
+    result = -1
+    while left <= right:
+        mid = left + (right - left)//2
+        if arr[mid] == target:
+            result = mid
+            right = mid - 1
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 2
+    return result
+print(binary_search1([1,1,1,2,3,4,4,5], 2))              
+            
+
+
+
+
+
+# find last occurrence of target
+def binary_search2(arr, target):
+    left,right = 0, len(arr)-1
+    result = -1
+    while left <= right:
+        mid = left + (right - left)//2
+        if arr[mid] == target:
+            result = mid
+            left = mid + 1
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return result  
+print(binary_search2([1,1,1,2,3,4,4,5], 1))
