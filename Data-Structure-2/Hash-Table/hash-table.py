fb34e59837fd332c5dd915ee3351d9e81c04fc79
affilpm <affilpm2004@gmail.com>
2024-07-19 22:17:36 +0530

    updated ds-2

--- Data-Structure-2/Hash-Table/hash-table.py
+++ Data-Structure-2/Hash-Table/hash-table.py
@@ -1,5 +1,44 @@
-# class hashtable:
-#     def __init__(self, size):
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(size)]
+    def hash_function(self,key):
+        return hash(key) % self.size    
+    
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+        self.table[index].append([key, value])
+
+    def search(self, key):
+        index = self.hash_function(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                return pair[1]
+        return None
+
+    def delete(self, key):
+        index = self.hash_function(key)
+        for i, pair in enumerate(self.table[index]):
+            if pair[0] == key:
+                del self.table[index][i]
+                return
+            
+    def display_all(self):
+        for i, bucket in enumerate(self.table):
+            if bucket:
+                print(f"Bucket {i}: {bucket}")        
+
+ht = HashTable(10)
+ht.insert("ss", "2")
+ht.insert("key2", "35")
+print(ht.search("key2"))  
+ht.delete("key1")
+# print(ht.search("key1")) 
+    
         
         
     
