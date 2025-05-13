9999eec060fe5790555241d76737524612dea3af
affilpm <affilpm2004@gmail.com>
2025-05-13 11:33:17 +0530

    added bubble sort in descending order

--- Data-Structure-2/Sorting/Bubble-Sort.py
+++ Data-Structure-2/Sorting/Bubble-Sort.py
@@ -16,7 +16,7 @@ def bubble_sort2(arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
+            if arr[j] < arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1],arr[j]
     return arr            
 arr = [3,5,6,2,4]
